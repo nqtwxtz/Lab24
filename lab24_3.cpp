319bbec6b78ad8ecf7807fdc38833d38025709c8
nqtwxtz <nut766933@gmail.com>
2026-03-09 11:41:30 +0700

    update

--- lab24_3.cpp
+++ lab24_3.cpp
@@ -1,9 +1,16 @@
 #include<iostream>
-
+#include<set>
 using namespace std;
 
-//Write function count() here.
-
+int count(int data[], int size) {
+    set<int> unique_numbers;
+    
+    for(int i = 0; i < size; i++) {
+        unique_numbers.insert(data[i]);
+    }
+    
+    return unique_numbers.size();
+}
 int main(){	
 	int data[] = {1,2,4,5,4,8,2,1,2,4,6,1,4,4,4,2,1,0,12}; 
 	
